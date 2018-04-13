d73f518517bb36a43dffe6c4a5536b060496fb4a
clockworkengineer <robert_tizzard@hotmail.com>
2018-04-13 18:51:00 +0100

    Refactor error handling.

--- FPE/filehandlers.py
+++ FPE/filehandlers.py
@@ -5,25 +5,6 @@ import csv
 import MySQLdb
 
 
-def _check_options(section, options):
-    
-    options_found = set()
-    
-    for option in section.keys():
-        if not option in options:
-            print('Invalid option name {}'.format(option))
-        else:
-            options_found.add(option)
-    
-    missing_options = set(options).difference(options_found)
-    
-    if missing_options != set():
-        print('Missing options {}'.format(list(missing_options)))
-        return(False)
-        
-    return(True)
-
-
 def _display_details(handler_section):
     print ('\n{} Handler [{}] running...'.format(handler_section['name'], handler_section['type']))
     for option in handler_section.keys():
@@ -35,24 +16,25 @@ def create_file_handler(handler_section):
     
     file_handler = None;
 
-    if handler_section['type'] == CopyFileHandler.__name__:
-        if _check_options(handler_section, CopyFileHandler.options):
+    try:
+        if handler_section['type'] == CopyFileHandler.__name__:
             file_handler = CopyFileHandler(handler_section)
-    elif handler_section['type'] == CSVFileToMySQLHandler.__name__:
-        if _check_options(handler_section, CSVFileToMySQLHandler.options):
+        elif handler_section['type'] == CSVFileToMySQLHandler.__name__:
             file_handler = CSVFileToMySQLHandler(handler_section)
-    else:
-        print ('Invalid file handler type {}.\n{} not started.'.format(handler_section['type'], handler_section['name']))
-
+        else:
+            print ('\nInvalid file handler type {}.\n{} not started.'.format(handler_section['type'], handler_section['name']))
+    
+    except KeyError as e:
+        print("\nMissing option {}.\n{} not started.".format(e, handler_section['name']))
+    
     return (file_handler)
 
     
 class CopyFileHandler(FileSystemEventHandler):
     
-    options = set(['name', 'type', 'watch', 'destination']);
-    
     def __init__(self, handler_section):
 
+        self.handler_name = handler_section['name']
         self.watch_folder = handler_section['watch']
         self.destination_folder = handler_section['destination']
         
@@ -60,27 +42,31 @@ class CopyFileHandler(FileSystemEventHandler):
          
     def on_created(self, event):
         
-        destination_path = event.src_path[len(self.watch_folder) + 1:]    
-        destination_path = os.path.join(self.destination_folder, destination_path)
-                
-        if os.path.isfile(event.src_path):            
-            if not os.path.exists(os.path.dirname(destination_path)):
-                os.makedirs(os.path.dirname(destination_path))
-            print ('Copying file {} to {}'.format(event.src_path, destination_path))
-            shutil.copy2(event.src_path, destination_path)
+        try:
             
-        elif os.path.isdir(event.src_path):           
-            if not os.path.exists(destination_path):
-                print ('Creating directory {}'.format(event.src_path))
-                os.makedirs(destination_path)    
+            destination_path = event.src_path[len(self.watch_folder) + 1:]    
+            destination_path = os.path.join(self.destination_folder, destination_path)
+                    
+            if os.path.isfile(event.src_path):            
+                if not os.path.exists(os.path.dirname(destination_path)):
+                    os.makedirs(os.path.dirname(destination_path))
+                print ('Copying file {} to {}'.format(event.src_path, destination_path))
+                shutil.copy2(event.src_path, destination_path)
+                
+            elif os.path.isdir(event.src_path):           
+                if not os.path.exists(destination_path):
+                    print ('Creating directory {}'.format(event.src_path))
+                    os.makedirs(destination_path)    
+
+        except Exception as e:
+            print("Error in handler {}: {}".format(self.handler_name, e))
 
  
 class CSVFileToMySQLHandler(FileSystemEventHandler):
     
-    options = set(['name', 'type', 'watch', 'server', 'user', 'password', 'database', 'table', 'key']);
-    
     def __init__(self, handler_section):
         
+        self.handler_name = handler_section['name']
         self.watch_folder = handler_section['watch']
         self.server = handler_section['server']
         self.user_name = handler_section['user']
@@ -126,32 +112,38 @@ class CSVFileToMySQLHandler(FileSystemEventHandler):
         
     def on_created(self, event):
         
-        db = MySQLdb.connect(self.server, self.user_name, self.user_password, self.database_name)
-        
-        cursor = db.cursor()
-
-        print ('Imorting CSV file {}'.format(event.src_path))
-
-        with open(event.src_path, 'r') as file_handle:
+        try:
             
-            csv_reader = csv.DictReader(file_handle)
-                       
-            for row in csv_reader:
-     
-                try:
-                    if self.key_name != '':
-                        sql = self.__update_row(self.table_name, self.key_name, row)
-                    else:
-                        sql = self.__insert_row(self.table_name, row)
-
-                    cursor.execute(sql)
-                    db.commit()
-                    
-                except (MySQLdb.Error, MySQLdb.Warning) as e:
-                    print (sql)
-                    print(e)
-                    db.rollback()
+            db = MySQLdb.connect(self.server, self.user_name, self.user_password, self.database_name)
+            
+            cursor = db.cursor()
+    
+            print ('Imorting CSV file {}'.format(event.src_path))
+    
+            with open(event.src_path, 'r') as file_handle:
+                
+                csv_reader = csv.DictReader(file_handle)
+                           
+                for row in csv_reader:
+         
+                    try:
+                        if self.key_name != '':
+                            sql = self.__update_row(self.table_name, self.key_name, row)
+                        else:
+                            sql = self.__insert_row(self.table_name, row)
+    
+                        cursor.execute(sql)
+                        db.commit()
                         
-        os.remove(event.src_path)
+                    except (MySQLdb.Error, MySQLdb.Warning) as e:
+                        print (sql)
+                        print(e)
+                        db.rollback()
+                        
+            os.remove(event.src_path)
+                        
+        except Exception as e:  
+            print("Error in handler {}: {}".format(self.handler_name, e))
         
-        db.close()
+        finally:     
+            db.close()
