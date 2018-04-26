0b9bd85c65d47f9f4d18282097d6710934854b53
clockworkengineer <robert_tizzard@hotmail.com>
2018-04-26 19:50:53 +0100

    Daily snapshot

--- GoogleDriveSync/GoogleDriveSync.py
+++ GoogleDriveSync/GoogleDriveSync.py
@@ -2,15 +2,18 @@
 
 """Synchronize google drive with local folder.
 
-At present it only provides a copy the google drive and local changes are not 
+At present it only a copies the google drive and local changes are not 
 reflected back on the drive.
 """
 
 from __future__ import print_function
 from  gdrive import GDrive
 import os
+import sys
 import logging
 import datetime
+import argparse
+import pytz
 
 __author__ = "Rob Tizzard"
 __copyright__ = "Copyright 20018"
@@ -21,18 +24,33 @@ __maintainer__ = "Rob Tizzard"
 __email__ = "robert_tizzard@hotmail.com"
 __status__ = "Pre-Alpha"
 
+_export_table = { 'application/vnd.google-apps.document' : ('application/pdf', 'pdf'),
+                  'application/vnd.google-apps.spreadsheet' : ('application/vnd.oasis.opendocument.spreadsheet', 'ods'),
+                  'application/vnd.google-apps.presentation' : ('application/vnd.oasis.opendocument.presentation', 'odp')
+                }
 
-def update_file(local_file, modifiedTime):
+_timezone = pytz.timezone('Europe/London')
+
+
+def update_file(local_file, modified_time):
     
-    if not os.path.exists(local_file):
-        print('File new{}'.format(local_file))
-        return(True)
-    
-    times_stamp = os.path.getmtime(local_file)
-    local_date_time = datetime.datetime.fromtimestamp(times_stamp)
-    remote_date_time = datetime.datetime.strptime(modifiedTime[:-5], '%Y-%m-%dT%H:%M:%S')
-    if remote_date_time > local_date_time:
-        print('File updated {}'.format(local_file))
+    try:
+
+        if not os.path.exists(local_file):
+            logging.info('File {} does not exist locally.'.format(local_file))
+            return(True)
+        
+        times_stamp = os.path.getmtime(local_file)
+        local_date_time = _timezone.localize(datetime.datetime.utcfromtimestamp(times_stamp))
+        
+        remote_date_time = _timezone.localize(datetime.datetime.strptime(modified_time[:-5], '%Y-%m-%dT%H:%M:%S'))
+        print(remote_date_time, local_date_time, local_file)
+        if remote_date_time > local_date_time:
+            logging.info('File {} needs updating locally.'.format(local_file))
+        
+    except Exception as e:
+        logging.error(e)
+        
     return(remote_date_time > local_date_time)
 
     
@@ -46,30 +64,47 @@ def local_file_path(file_name, file_extension=None):
     return(local_file)
 
     
-def traverse(my_drive, file_list):
+def traverse_drive(my_drive, file_list, refresh=False):
     
     for file_data in file_list:
 
-        if file_data['mimeType'] == 'application/vnd.google-apps.folder':
-            query = "('{}' in parents) and (not trashed)".format(file_data['id'])
-            list_results = my_drive.file_list(query, file_fields='name, id, parents, mimeType, modifiedTime')
-            if not os.path.exists(file_data['name']):
-                os.mkdir(file_data['name'])
-            os.chdir(file_data['name'])
-            traverse(my_drive, list_results)
-            os.chdir('..')
-        elif file_data['mimeType'] == 'application/vnd.google-apps.document':
-            local_file = local_file_path(file_data['name'], 'pdf')
-            if update_file(local_file, file_data['modifiedTime']):
-                my_drive.file_export(file_data['id'], local_file, 'application/pdf', 'pdf')
-        elif file_data['mimeType'] == 'application/vnd.google-apps.spreadsheet':
-            local_file = local_file_path(file_data['name'], 'xlsx')
-            if update_file(local_file, file_data['modifiedTime']):
-                my_drive.file_export(file_data['id'], local_file, 'application/pdf', 'xlsx')
-        else:
-            local_file = local_file_path(file_data['name'])
-            if update_file(local_file, file_data['modifiedTime']):
-                my_drive.file_download(file_data['id'], local_file)
+        try:
+            
+            if file_data['mimeType'] == 'application/vnd.google-apps.folder':
+                query = "('{}' in parents) and (not trashed)".format(file_data['id'])
+                list_results = my_drive.file_list(query, file_fields='name, id, parents, mimeType, modifiedTime')
+                if not os.path.exists(file_data['name']):
+                    os.mkdir(file_data['name'])
+                os.chdir(file_data['name'])
+                traverse_drive(my_drive, list_results, refresh)
+                os.chdir('..')
+            elif file_data['mimeType'] in _export_table:
+                export_tuple = _export_table[file_data['mimeType']]
+                local_file = local_file_path(file_data['name'], export_tuple[1])
+                if refresh or update_file(local_file, file_data['modifiedTime']):
+                    my_drive.file_export(file_data['id'], local_file, export_tuple[0])          
+            else:
+                local_file = local_file_path(file_data['name'])
+                if refresh or update_file(local_file, file_data['modifiedTime']):
+                    my_drive.file_download(file_data['id'], local_file)
+                
+        except Exception as e:
+            logging.error(e)
+
+
+def load_arguments():
+    """Load and parse command line arguments"""
+    
+    parser = argparse.ArgumentParser(description='Synchronize Google Drive with a local folder')
+    parser.add_argument('folder', help='Local folder')
+    parser.add_argument('-r', '--refresh', action='store_true', help='Refresh all files.')
+    parser.add_argument('-s', '--scope', default='https://www.googleapis.com/auth/drive.readonly', help='Google Drive API Scope')
+    parser.add_argument('-e', '--secrets', default='client_secret.json', help='Google API secrets file')
+    parser.add_argument('-c', '--credentials', default='credentials.json', help='Google API credtials file')
+
+    arguments = parser.parse_args()
+    
+    return(arguments)
 
 ####################
 # Main Entry Point #
@@ -78,22 +113,38 @@ def traverse(my_drive, file_list):
 
 def main():
     
-#     logging.basicConfig(level=logging.INFO)
- 
-    my_drive = GDrive('https://www.googleapis.com/auth/drive.readonly',
-                      'client_secret.json', 'credentials.json')
-    
-    my_drive.authorize()
-    
-    my_drive.start_service()
-    
-    list_results = my_drive.file_list("('root' in parents) and (not trashed)",
-                                      file_fields='name, id, parents, mimeType, modifiedTime')
-    
-    os.chdir('/home/robt/ftproot')
-    
-    traverse(my_drive, list_results)
+    try:
+        
+        logging.basicConfig(level=logging.INFO)
+        
+        arguments = load_arguments()
 
+        logging.info('GooggleDriveSync: Sychronizing to local folder {}.'.format(arguments.folder))
+        
+        if arguments.refresh:
+            logging.info('Refeshing whole Google drive tree locally.')
+     
+        my_drive = GDrive(arguments.scope, arguments.secrets, arguments.credentials)
+        
+        my_drive.authorize()
+        
+        my_drive.start_service()
+        
+        top_level = my_drive.file_list("('root' in parents) and (not trashed)",
+                                          file_fields='name, id, parents, mimeType, modifiedTime')
+        
+        if not os.path.exists(arguments.folder):
+            os.makedirs(arguments.folder)
+            
+        os.chdir(arguments.folder)
+        
+        traverse_drive(my_drive, top_level, arguments.refresh)
+    
+        logging.info('GooggleDriveSync: End of drive Sync'.format(arguments.folder))
 
+    except Exception as e:
+        logging.error(e)
+
+        
 if __name__ == '__main__':
     main()
--- GoogleDriveSync/gdrive.py
+++ GoogleDriveSync/gdrive.py
@@ -1,6 +1,6 @@
 """Class for accessing google drive.
 
-Class to connect, upload, download and interrogate files on google drive.
+Class to connect, upload, download, maipulate and interrogate files on google drive.
 """
 
 from __future__ import print_function
@@ -21,7 +21,9 @@ __maintainer__ = "Rob Tizzard"
 __email__ = "robert_tizzard@hotmail.com"
 __status__ = "Pre-Alpha"
 
+logging.getLogger("googleapiclient").setLevel(logging.WARNING)
 
+   
 class GDrive(object):
     
     def __init__(self, scope, secrets_file, credentials_file):
@@ -41,15 +43,16 @@ class GDrive(object):
                 flow = client.flow_from_clientsecrets(self.secrets_file, self.scope)
                 self.credentials = tools.run_flow(flow, store)
         except Exception as e:
-            print(e)
+            logging.error(e)
         
     def start_service(self):
         
         try :
             self.drive_service = build('drive', 'v3',
-                                       http=self.credentials.authorize(Http()))
+                                       http=self.credentials.authorize(Http()),
+                                       cache_discovery=False)
         except Exception as e:
-            print(e)
+            logging.error(e)
 
     def file_list(self, query='', max_files=100, file_fields='name, id'):
         
@@ -77,28 +80,30 @@ class GDrive(object):
                     more_files_to_list = False
                 
         except Exception as e:
-            print(e)
+            logging.error(e)
          
         finally:
             return (files_returned)
         
-    def file_upload(self, local_file_path, parent_id=None):
+    def file_upload(self, local_file, parent_id=None):
         
-        file_metadata = {'name': os.path.basename(local_file_path)}
+        file_metadata = {'name': os.path.basename(local_file)}
         
         if parent_id:
             file_metadata['parents'] = [ parent_id ]
 
-        file_mime_type = magic.from_file(local_file_path, mime=True)
+        file_mime_type = magic.from_file(local_file, mime=True)
         
-        media = MediaFileUpload(local_file_path,
+        media = MediaFileUpload(local_file,
                             mimetype=file_mime_type)
         
         result = self.drive_service.files().create(body=file_metadata,
                                         media_body=media,
                                         fields='id').execute()
+                                        
+        return(result)
         
-    def file_download(self, file_id, local_file_path):
+    def file_download(self, file_id, local_file):
         
         request = self.drive_service.files().get_media(fileId=file_id)
         file_handle = io.BytesIO()
@@ -107,11 +112,11 @@ class GDrive(object):
         while not done:
             done = downloader.next_chunk()
             
-        with io.open(local_file_path, 'wb') as f:
+        with io.open(local_file, 'wb') as f:
             file_handle.seek(0)
             f.write(file_handle.read())
 
-        logging.info('Downloaded file {} to {}'.format(file_id, local_file_path))
+        logging.info('Downloaded file {} to {}'.format(file_id, local_file))
 
     def folder_create(self, folder_name):
         
@@ -125,13 +130,13 @@ class GDrive(object):
                                             
         return(folder_data['id'])
     
-    def file_get_metadata(self, file_id, file_fields='*'):
+    def file_get_metadata(self, file_id, file_fields='name, id'):
         
         data = self.drive_service.files().get(fileId=file_id, fields=file_fields).execute()
         
         return(data)
     
-    def file_export(self, file_id, local_file_path, mime_type, file_extension):
+    def file_export(self, file_id, local_file, mime_type):
         
         request = self.drive_service.files().export_media(fileId=file_id, mimeType=mime_type)
 
@@ -141,9 +146,9 @@ class GDrive(object):
         while not done:
             done = downloader.next_chunk()
             
-        with io.open(local_file_path, 'wb') as f:
+        with io.open(local_file, 'wb') as f:
             file_handle.seek(0)
             f.write(file_handle.read())
      
-        logging.info('Exported file {} to {}'.format(file_id, local_file_path))
+        logging.info('Exported file {} to {}'.format(file_id, local_file))
 
