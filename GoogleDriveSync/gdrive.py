7df9108433943ea3a3619701743ada372f0c21f6
clockworkengineer <robert_tizzard@hotmail.com>
2018-05-05 14:20:07 +0100

    Use _ for private attributes in GDrive class.

--- GoogleDriveSync/gdrive.py
+++ GoogleDriveSync/gdrive.py
@@ -36,7 +36,7 @@ def GAuthorize(scope, secrets_file, credentials_file):
     Args:
         scope:            Google drive API scope
         secrets_file:     Application secrets file
-        credentials_file: Application credentials file
+        credentials_file: Application _credentials file
     
     Returns: 
         Credentials(token) for acccessing Google drive.
@@ -62,19 +62,18 @@ class GDrive(object):
     Open up service to Google drive to list/manipulate/upload/download files.
     
     Attrubutes:
-    credentials:      Application credentials(token) for accessing drive
-    drive_srvice:     Drive Service
-    start_page_token: Saved start page token used to get changes
+    _credentials:      Application _credentials(token) for accessing drive
+    _drive_srvice:     Drive Service
+    _start_page_token: Saved start page token used to get changes
     """
     
     def __init__(self, credentials):
         
-        self.credentials = credentials
-        self.drive_service = None
-        self.start_page_token = None
+        self._credentials = credentials
+        self._start_page_token = None
 
-        self.drive_service = build('drive', 'v3',
-                                    http=self.credentials.authorize(Http()),
+        self._drive_service = build('drive', 'v3',
+                                    http=self._credentials.authorize(Http()),
                                     cache_discovery=False)
 
     def file_list(self, query='', max_files=100, file_fields='name, id'):
@@ -89,7 +88,7 @@ class GDrive(object):
             list_fields = 'nextPageToken, incompleteSearch, files({})'.format(file_fields)
             while more_files_to_list:
                 
-                list_results = self.drive_service.files().list(pageSize=max_files,
+                list_results = self._drive_service.files().list(pageSize=max_files,
                                                           q=query,
                                                           pageToken=next_page_token,
                                                           fields=list_fields).execute()
@@ -111,7 +110,75 @@ class GDrive(object):
             logging.debug('List File (Length={}).Items={}'.format(len(files_returned), files_returned))
             
             return (files_returned)
+
+    def folder_create(self, folder_name, parent_id=None):
+        """Create a folder on google drive"""
         
+        try:
+            
+            result = None
+            
+            file_metadata = {
+            'name': folder_name,
+            'mimeType': 'application/vnd.google-apps.folder'
+            }
+    
+            if parent_id:
+                file_metadata['parents'] = [ parent_id ]
+    
+            folder_data = self._drive_service.files().create(body=file_metadata,
+                                                            fields='id').execute()
+                                                            
+            result = folder_data['id']
+
+        except Exception as e:
+            logging.error(e)
+        
+        finally:                       
+            return(result)                                           
+
+    def file_get_metadata(self, file_id, file_fields='name, id'):
+        """Return metadata associated with google drivefile."""
+        
+        try:
+            
+            result = self._drive_service.files().get(fileId=file_id, fields=file_fields).execute()
+        
+        except Exception as e:
+            logging.error(e)
+            
+        return(result)
+    
+    def file_download(self, file_id, local_file, mime_type=None):
+        """Download/Export google drive file with id to local file system."""
+        
+        try:
+            
+            # If mime type set then exporting google applciation file so export (convert)
+            
+            if mime_type:
+                request = self._drive_service.files().export_media(fileId=file_id, mimeType=mime_type[0])
+                
+            # None google file so just download
+            
+            else:
+                request = self._drive_service.files().get_media(fileId=file_id)
+                       
+            file_handle = io.BytesIO()
+            downloader = MediaIoBaseDownload(file_handle, request)
+            done = False
+            while not done:
+                done = downloader.next_chunk()
+                
+            with io.open(local_file, 'wb') as f:
+                file_handle.seek(0)
+                f.write(file_handle.read())
+         
+            logging.info('Downloaded file {} to {}'.format(file_id, local_file))
+        
+        except Exception as e:
+            logging.error(e)
+
     def file_upload(self, local_file, parent_id=None):
         """Upload local file to google drive returning its file id."""
         
@@ -129,7 +196,7 @@ class GDrive(object):
             media = MediaFileUpload(local_file,
                                 mimetype=file_mime_type)
             
-            result = self.drive_service.files().create(body=file_metadata,
+            result = self._drive_service.files().create(body=file_metadata,
                                             media_body=media,
                                             fields='id').execute()
         except Exception as e:
@@ -137,75 +204,7 @@ class GDrive(object):
         
         finally:                       
             return(result)
-
-    def folder_create(self, folder_name, parent_id=None):
-        """Create a folder on google drive"""
         
-        try:
-            
-            result = None
-            
-            file_metadata = {
-            'name': folder_name,
-            'mimeType': 'application/vnd.google-apps.folder'
-            }
-    
-            if parent_id:
-                file_metadata['parents'] = [ parent_id ]
-    
-            folder_data = self.drive_service.files().create(body=file_metadata,
-                                                            fields='id').execute()
-                                                            
-            result = folder_data['id']
-
-        except Exception as e:
-            logging.error(e)
-        
-        finally:                       
-            return(result)                                           
-
-    def file_get_metadata(self, file_id, file_fields='name, id'):
-        """Return metadata associated with google drivefile."""
-        
-        try:
-            
-            result = self.drive_service.files().get(fileId=file_id, fields=file_fields).execute()
-        
-        except Exception as e:
-            logging.error(e)
-            
-        return(result)
-    
-    def file_download(self, file_id, local_file, mime_type=None):
-        """Download/Export google drive file with id to local file system."""
-        
-        try:
-            
-            # If mime type set then exporting google applciation file so export (convert)
-            
-            if mime_type:
-                request = self.drive_service.files().export_media(fileId=file_id, mimeType=mime_type[0])
-                
-            # None google file so just download
-            
-            else:
-                request = self.drive_service.files().get_media(fileId=file_id)
-                       
-            file_handle = io.BytesIO()
-            downloader = MediaIoBaseDownload(file_handle, request)
-            done = False
-            while not done:
-                done = downloader.next_chunk()
-                
-            with io.open(local_file, 'wb') as f:
-                file_handle.seek(0)
-                f.write(file_handle.read())
-         
-            logging.info('Downloaded file {} to {}'.format(file_id, local_file))
-        
-        except Exception as e:
-            logging.error(e)
-
     def retrieve_all_changes(self):
         """Retrieve list of changes to google drive since last call"""
     
@@ -213,22 +212,22 @@ class GDrive(object):
             
             changes = []
             
-            if not self.start_page_token:
-                response = self.drive_service.changes().getStartPageToken().execute()
-                self.start_page_token = response.get('startPageToken')
+            if not self._start_page_token:
+                response = self._drive_service.changes().getStartPageToken().execute()
+                self._start_page_token = response.get('startPageToken')
             
-            page_token = self.start_page_token
+            page_token = self._start_page_token
             
             while page_token is not None:
                 
-                response = self.drive_service.changes().list(pageToken=page_token,
+                response = self._drive_service.changes().list(pageToken=page_token,
                                                     spaces='drive').execute()
                                                     
                 for change in response.get('changes'):
                     changes.append(change)
                  
                 if 'newStartPageToken' in response:
-                    self.start_page_token = response.get('newStartPageToken')
+                    self._start_page_token = response.get('newStartPageToken')
                  
                 page_token = response.get('nextPageToken')
             
