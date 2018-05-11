0032e7606ff180a22954729359e5392ddf99cae2
clockworkengineer <robert_tizzard@hotmail.com>
2018-05-11 17:32:49 +0100

    Alter handlers __init__.py so that it adds handlers to sys.path.

--- FPE/handlers/__init__.py
+++ FPE/handlers/__init__.py
@@ -1,3 +1,11 @@
+# Make sure handler directory is on sys,path
+
+import sys
+import os
+
+if os.path.join(os.getcwd(), 'handlers') not in sys.path:
+    sys.path.insert(1, os.path.join(os.getcwd(), 'handlers'))
+
 from CopyFile import CopyFile
 from CSVFileToMySQL import CSVFileToMySQL
 from CSVFileToSQLite import CSVFileToSQLite
