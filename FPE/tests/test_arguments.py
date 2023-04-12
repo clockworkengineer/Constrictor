5060e864b37ded3859e989d8e9807931b8670f30
clockworkengineer <robert_tizzard@hotmail.com>
2023-04-12 08:34:32 +0100

    Fix usage of path.join in tests.

--- FPE/tests/test_arguments.py
+++ FPE/tests/test_arguments.py
@@ -1,13 +1,18 @@
-from core.arguments import Arguments, ArgumentsError
 from pytest import raises
 import os
 
+from core.arguments import Arguments, ArgumentsError
 
 def test_arguments_with_existing_json_file():
-    arg = Arguments([os.path.join(os.getcwd(), "FPE\\tests\\test.json")])
-    assert arg.file == os.path.join(os.getcwd(), "FPE\\tests\\test.json")
+    arg = Arguments([os.path.join(os.getcwd(), "FPE","tests","test.json")])
+    assert arg.file == os.path.join(os.getcwd(), "FPE", "tests","test.json")
 
 
 def test_arguments_with_nonexistant_json__file():
     with raises(ArgumentsError):
-        _ = Arguments([os.path.join(os.getcwd(), "FPE\\tests\\test.jsn")])
+        _ = Arguments([os.path.join(os.getcwd(), "FPE", "tests", "test.jsn")])
+
+
+def test_arguments_output_of_help():
+    # _ = Arguments(["-h"])
+    assert(True)
