4664badcf783cf09a3296bee445305ee019edfe2
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-04-15 09:25:34 +0100

    Add more config tests and move test files.

--- FPE/tests/test_config.py
+++ FPE/tests/test_config.py
@@ -6,21 +6,37 @@ from core.config import Config, ConfigError
 
 
 class TestConfig:
-    
+
     def test_config_with_valid_json_file(self):
-        config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test.json")])).get_config()
+        config = Config(Arguments(
+            [os.path.join(os.getcwd(), "FPE", "tests", "json", "test.json")])).get_config()
         assert config["plugins"][0] == "plugins.fileannouncer_handler"
-        
+
     def test_config_with_invalid_json_file(self):
         with raises(ConfigError):
-            config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test_invalid.json")]))
-            
+            config = Config(Arguments(
+                [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_invalid.json")]))
+
     def test_config_with_no_plugin_key(self):
         with raises(ConfigError):
-            config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test_noplugin_key.json")]))
+            config = Config(Arguments(
+                [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_noplugin_key.json")]))
             config.validate()
-            
+
     def test_config_with_no_watchers_key(self):
         with raises(ConfigError):
-            config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test_nowatchers_key.json")]))
+            config = Config(Arguments(
+                [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_nowatchers_key.json")]))
+            config.validate()
+
+    def test_config_with_watcher_name_missing(self):
+        with raises(ConfigError):
+            config = Config(Arguments([os.path.join(
+                os.getcwd(), "FPE", "tests", "json", "test_watcher_name_missing.json")]))
+            config.validate()
+            
+    def test_config_with_watcher_type_missing(self):
+        with raises(ConfigError):
+            config = Config(Arguments([os.path.join(
+                os.getcwd(), "FPE", "tests", "json", "test_watcher_type_missing.json")]))
             config.validate()
