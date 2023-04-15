418d876da1359712045432aedbcbbec24a4f6e2d
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-04-15 09:16:21 +0100

    Add more config tests.

--- FPE/tests/test_arguments.py
+++ FPE/tests/test_arguments.py
@@ -8,17 +8,17 @@ class TestArguments:
 
     def test_arguments_with_existing_json_file(self):
         arg = Arguments(
-            [os.path.join(os.getcwd(), "FPE", "tests", "test.json")])
+            [os.path.join(os.getcwd(), "FPE", "tests", "json", "test.json")])
         assert arg.file == os.path.join(
-            os.getcwd(), "FPE", "tests", "test.json")
+            os.getcwd(), "FPE", "tests", "json", "test.json")
 
     def test_arguments_with_nonexistant_json__file(self):
         with raises(ArgumentsError):
             _ = Arguments(
-                [os.path.join(os.getcwd(), "FPE", "tests", "test.jsn")])
+                [os.path.join(os.getcwd(), "FPE", "tests", "json", "test.jsn")])
 
     def test_arguments_output_of_help(self, capsys):
         with raises(SystemExit):
             _ = Arguments(["-h"])
         out, _ = capsys.readouterr()
-        assert out == 'usage: __main__.py [-h] file\n\nProcess files copied into watch folder with a custom handler(s).\n\npositional arguments:\n  file        Configuration file\n\noptional arguments:\n  -h, --help  show this help message and exit\n'
+        assert out.startswith("usage")
--- FPE/tests/test_config.py
+++ FPE/tests/test_config.py
@@ -8,10 +8,19 @@ from core.config import Config, ConfigError
 class TestConfig:
     
     def test_config_with_valid_json_file(self):
-        config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","test.json")])).get_config()
+        config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test.json")])).get_config()
         assert config["plugins"][0] == "plugins.fileannouncer_handler"
         
     def test_config_with_invalid_json_file(self):
         with raises(ConfigError):
-            config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","test_invalid.json")]))
-        
+            config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test_invalid.json")]))
+            
+    def test_config_with_no_plugin_key(self):
+        with raises(ConfigError):
+            config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test_noplugin_key.json")]))
+            config.validate()
+            
+    def test_config_with_no_watchers_key(self):
+        with raises(ConfigError):
+            config = Config(Arguments([os.path.join(os.getcwd(), "FPE","tests","json", "test_nowatchers_key.json")]))
+            config.validate()
