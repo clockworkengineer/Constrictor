31a8687064f31ccbfb5e810ad426bc5db947b1b6
clockworkengineer <robert_tizzard@hotmail.com>
2023-04-17 08:26:48 +0100

    Add possible test lists

--- FPE/tests/test_plugin.py
+++ FPE/tests/test_plugin.py
@@ -5,3 +5,7 @@ from core.plugin import PluginLoader
 
 class TestPlugin:
     pass
+    # Test plugin loader called with zero length list
+    # Test plugin loader called non existant handler type
+    # Test plugin loader called with plugin with no register
+    # Test plugin loader called with a valid plugin
--- FPE/tests/test_watcher.py
+++ FPE/tests/test_watcher.py
@@ -5,3 +5,9 @@ from core.watcher import Watcher, WatcherError
 
 class TestWatcher:
     pass
+    # Test watcher with None for config passed in
+    # Test watchef with invalid confg passed in
+    # Test watcher with valid config pass in
+    # Test watcher initial state is stopped
+    # Test watcher start
+    # Test watcher stop
