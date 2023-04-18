b5ab8c31f1c0e0439b6144ee16de547ff0dc283f
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-04-18 18:16:39 +0100

    Refactor watcher tests.

--- FPE/tests/test_core_watcher.py
+++ FPE/tests/test_core_watcher.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 import time
+from typing import Any
 
 from core.arguments import Arguments
 from core.config import Config
@@ -9,71 +10,55 @@ from core.factory import Factory
 from builtin.copyfile_handler import CopyFileHandler
 from builtin.sftp_copyfile_handler import SFTPCopyFileHandler
 
+
 @pytest.fixture()
-def reset_factory():
-    Factory.handler_creation_funcs.clear()
-    yield
+def reset_factory_and_return_config() -> dict[str, Any]:
     
+    Factory.handler_creation_funcs.clear()
+    Factory.register("CopyFile", CopyFileHandler)
+    Factory.register("SFTPCopyFile", SFTPCopyFileHandler)
+
+    config = Config(Arguments(
+        [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_valid.json")])).get_config()
+    
+    yield config["watchers"][0]
+
+
 class TestCoreWatcher:
-    # Test watcher with None for config passed in
-    def test_watcher_with_config_of_none(self,reset_factory):
+
+    def test_watcher_with_config_of_none(self, reset_factory_and_return_config):
+        
         with pytest.raises(WatcherError):
             _ = Watcher(None)
 
-    def test_watcher_with_a_valid_config(self, reset_factory):
+    def test_watcher_with_a_valid_config(self, reset_factory_and_return_config):
 
-        Factory.register("CopyFile", CopyFileHandler)
-        Factory.register("SFTPCopyFile", SFTPCopyFileHandler)
-
-        config = Config(Arguments(
-            [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_valid.json")])).get_config()
-
-        watcher = Watcher(config["watchers"][0])
+        watcher = Watcher(reset_factory_and_return_config)
 
         assert watcher != None
-        
-    def test_watcher_initial_state_stopped(self, reset_factory):
 
-        Factory.register("CopyFile", CopyFileHandler)
-        Factory.register("SFTPCopyFile", SFTPCopyFileHandler)
+    def test_watcher_initial_state_stopped(self, reset_factory_and_return_config):
 
-        config = Config(Arguments(
-            [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_valid.json")])).get_config()
-
-        watcher = Watcher(config["watchers"][0])
+        watcher = Watcher(reset_factory_and_return_config)
 
         assert watcher.started == False
-        
-    def test_watcher_started(self, reset_factory):
 
-        Factory.register("CopyFile", CopyFileHandler)
-        Factory.register("SFTPCopyFile", SFTPCopyFileHandler)
+    def test_watcher_started(self, reset_factory_and_return_config):
 
-        config = Config(Arguments(
-            [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_valid.json")])).get_config()
+        watcher = Watcher(reset_factory_and_return_config)
 
-        watcher = Watcher(config["watchers"][0])
-        
         watcher.start()
 
         assert watcher.started == True
-        
-    def test_watcher_started_then_stopped(self, reset_factory):
 
-        Factory.register("CopyFile", CopyFileHandler)
-        Factory.register("SFTPCopyFile", SFTPCopyFileHandler)
+    def test_watcher_started_then_stopped(self, reset_factory_and_return_config):
 
-        config = Config(Arguments(
-            [os.path.join(os.getcwd(), "FPE", "tests", "json", "test_valid.json")])).get_config()
+        watcher = Watcher(reset_factory_and_return_config)
 
-        watcher = Watcher(config["watchers"][0])
-        
         watcher.start()
         time.sleep(1)
         watcher.stop()
 
         assert watcher.started == False
 
-
     # Test watcher with invalid confg passed in
-   
