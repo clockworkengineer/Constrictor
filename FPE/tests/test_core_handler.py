1097e1406af31606e8875948de85a18a62cb732f
clockworkengineer <robert_tizzard@hotmail.com>
2023-04-25 07:08:28 +0100

    Add future tests comment.

--- FPE/tests/test_core_handler.py
+++ FPE/tests/test_core_handler.py
@@ -3,9 +3,10 @@ import pytest
 from core.handler import Handler
 
 
-# Currently not tests exist for this
-
 class TestCoreHandler:
     pass
 
-    
+    # Test Handler normalize path
+    # Test Handler create path
+    # Test Handler create local destination
+    # Test Handler setup path
