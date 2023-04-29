49e5c312e8aabaaa5688cb618935c42635a36a62
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-04-29 09:50:57 +0100

    Add more tests.

--- FPE/tests/test_builtin_copyfile_handler.py
+++ FPE/tests/test_builtin_copyfile_handler.py
@@ -1,5 +1,6 @@
 import pytest
 import pathlib
+import shutil
 import tempfile
 from typing import Any
 
@@ -7,47 +8,84 @@ from core.error import FPEError
 from core.handler import Handler
 from builtin.copyfile_handler import CopyFileHandler, CopyFileHandlerError
 
+
 class Fixture:
-    source_path : pathlib.Path
-    destination_path : pathlib.Path
+    source_path: pathlib.Path
+    destination_path: pathlib.Path
     config: dict[str, Any] = {}
-    
+
+
 @pytest.fixture()
 def setup_source_destination() -> Fixture:
     fixture: Fixture = Fixture()
     with tempfile.TemporaryDirectory() as directory_name:
-            source_directory_path: pathlib.Path = pathlib.Path(
-                directory_name) / "watchers" / "source"
-            destination_directory_path: pathlib.Path = pathlib.Path(
-                directory_name) / "watchers" / "destination"
-            fixture.source_path = source_directory_path
-            fixture.destination_path = destination_directory_path
-    if fixture.source_path.exists():
-        fixture.setup_source_destination.source_path.rmdir()
-    if fixture.destination_path.exists():
-        fixture.setup_source_destination.destination_path.rmdir()
-    fixture.config["source"] = str(fixture.source_path)
-    fixture.config["destination"] = str(fixture.destination_path)
-    return fixture
-    
+        source_directory_path: pathlib.Path = pathlib.Path(
+            directory_name) / "watchers" / "source"
+        destination_directory_path: pathlib.Path = pathlib.Path(
+            directory_name) / "watchers" / "destination"
+        fixture.source_path = source_directory_path
+        fixture.destination_path = destination_directory_path
+        if fixture.source_path.exists():
+            fixture.source_path.rmdir()
+        if fixture.destination_path.exists():
+            fixture.destination_path.rmdir()
+        fixture.config["source"] = str(fixture.source_path)
+        fixture.config["destination"] = str(fixture.destination_path)
+        fixture.config["deletesource"] = False
+        fixture.config["recursive"] = True
+    yield fixture
+    shutil.rmtree(fixture.source_path)
+    shutil.rmtree(fixture.destination_path)
+
 
 class TestBuiltinCopyFileHandler:
-    
 
     # Test CopyFileHandler checks for None passed in as config
     def test_builtin_copyfile_handler_pass_none_as_config(self) -> None:
         with pytest.raises(FPEError):
-            handdler : Handler = CopyFileHandler(None)
+            handdler: Handler = CopyFileHandler(None)  # type: ignore
 
     # Test CopyFileHandler creates non-existant source
     def test_buitin_handler_create_non_existant_source(self, setup_source_destination) -> None:
-        setup_source_destination.destination_path.mkdir(parents=True,  exist_ok=True)
-        assert True
-        
-        
-    # Test CopyFileHandler create non-existant destinati on
+        setup_source_destination.destination_path.mkdir(
+            parents=True,  exist_ok=True)
+        handler = CopyFileHandler(setup_source_destination.config)
+        assert setup_source_destination.source_path.exists()
+
+    # Test CopyFileHandler create non-existant destination
+    def test_buitin_handler_create_non_existant_destination(self, setup_source_destination) -> None:
+        setup_source_destination.source_path.mkdir(
+            parents=True,  exist_ok=True)
+        handler = CopyFileHandler(setup_source_destination.config)
+        assert setup_source_destination.destination_path.exists()
+
     # Test CopyFileHandler single file copied into source then copied to destination
+
+    def test_buitin_handler_copy_source_to_destination(self, setup_source_destination) -> None:
+        setup_source_destination.source_path.mkdir(
+            parents=True,  exist_ok=True)
+        handler = CopyFileHandler(setup_source_destination.config)
+        source_file = setup_source_destination.source_path / "test.txt"
+        destination_file = setup_source_destination.destination_path / "test.txt"
+        source_file.touch()
+        assert not destination_file.exists()
+        handler.process(str(source_file))
+        assert destination_file.exists()
+        assert source_file.exists()
+
     # Test CopyFileHandler single file copied into source then copied to destination and source file deleted
+    def test_buitin_handler_copy_source_to_destination_deleting_source(self, setup_source_destination) -> None:
+        setup_source_destination.source_path.mkdir(
+            parents=True,  exist_ok=True)
+        setup_source_destination.config["deletesource"] = True
+        handler = CopyFileHandler(setup_source_destination.config)
+        source_file = setup_source_destination.source_path / "test.txt"
+        destination_file = setup_source_destination.destination_path / "test.txt"
+        source_file.touch()
+        assert not destination_file.exists()
+        handler.process(str(source_file))
+        assert destination_file.exists()
+        assert not source_file.exists()
+
     # Test CopyFileHandler a whole directory structure copied into source then copied to destination
     # Test CopyFileHandler copies a whole directory structure copied into source then copied to destination amd source files deleted
-     
--- FPE/tests/test_core_handler.py
+++ FPE/tests/test_core_handler.py
@@ -7,13 +7,10 @@ from core.handler import Handler
 
 
 class TestCoreHandler:
-    # Test Handler normalize path
 
     def test_core_handler_normalise_path(self) -> None:
         assert Handler.normalize_path(
             "./watcher/source") == str(pathlib.Path.cwd() / "watcher" / "source")
-
-    # Test Handler create path
 
     def test_core_handler_create_path(self) -> None:
         with tempfile.TemporaryDirectory() as directory_name:
@@ -24,41 +21,41 @@ class TestCoreHandler:
             assert temporary_directory_path.exists()
             temporary_directory_path.rmdir()
 
-    # Test Handler create local destination
     def test_core_handler_create_local_destination(self) -> None:
         config: dict[str, Any] = {}
-        source_path:pathlib.Path = pathlib.Path(Handler.normalize_path("./watcher/source"))
-        destination_path: pathlib.Path = pathlib.Path(Handler.normalize_path("./watcher/destination"))
+        source_path: pathlib.Path = pathlib.Path(
+            Handler.normalize_path("./watcher/source"))
+        destination_path: pathlib.Path = pathlib.Path(
+            Handler.normalize_path("./watcher/destination"))
         source_file_path: pathlib.Path = source_path / "dir1" / "dir2" / "source.txt"
         config["source"] = str(source_path)
         config["destination"] = str(destination_path)
-        assert Handler.create_local_destination(str(source_file_path), config) == str(destination_path / "dir1" / "dir2" / "source.txt" )
-    # Test Handler setup path
-    
-    def test_core_handler_setup_path_doesnt_exist(self) -> None:
-        config : dict[str, Any] = {}
-        config["source"] = "./watchers/source"
-        source_path:pathlib.Path = pathlib.Path(config["source"])
+        assert Handler.create_local_destination(str(source_file_path), config) == str(
+            destination_path / "dir1" / "dir2" / "source.txt")
 
+    def test_core_handler_setup_path_doesnt_exist(self) -> None:
+        config: dict[str, Any] = {}
+        config["source"] = "./watchers/source"
+        source_path: pathlib.Path = pathlib.Path(config["source"])
         if source_path.exists():
             source_path.rmdir()
         Handler.setup_path(config, "source")
         assert source_path.exists()
         source_path.rmdir()
-        
+
     def test_core_handler_setup_path_does_exist(self) -> None:
-        config : dict[str, Any] = {}
+        config: dict[str, Any] = {}
         config["source"] = "./watchers/source"
-        source_path:pathlib.Path = pathlib.Path(config["source"])
+        source_path: pathlib.Path = pathlib.Path(config["source"])
         source_path.mkdir(parents=True, exist_ok=True)
         assert source_path.exists()
         Handler.setup_path(config, "source")
         assert source_path.exists()
         source_path.rmdir()
-        
+
     def test_core_handler_setup_path_with_invalid_config_key(self) -> None:
-        config : dict[str, Any] = {}
+        config: dict[str, Any] = {}
         config["source"] = "./watchers/source"
-        source_path:pathlib.Path = pathlib.Path(config["source"])
+        source_path: pathlib.Path = pathlib.Path(config["source"])
         with pytest.raises(KeyError):
             Handler.setup_path(config, "destination")
