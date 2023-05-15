b09551f70cb580182c86a8d90381cff2d7bc63f8
clockworkengineer <robert_tizzard@hotmail.com>
2023-05-15 09:38:43 +0100

    SNapshot.

--- FPE/core/constants.py
+++ FPE/core/constants.py
@@ -9,6 +9,6 @@ CONFIG_PLUGINS : Final[str] = "plugins"
 CONFIG_DELETESOURCE  : Final[str] = "deletesoure"
 CONFIG_EXITONFAILURE : Final[str] = "exitonfailure"
 
-CONFIG_MANDATORY_KEYS: Tuple[str, ...] = (CONFIG_PLUGINS, CONFIG_WATCHERS)
-CONFIG_WATCHER_MANDATORY_KEYS: Tuple[str, ...] = (
+CONFIG_MANDATORY_KEYS: Final[Tuple[str, ...]] = (CONFIG_PLUGINS, CONFIG_WATCHERS)
+CONFIG_WATCHER_MANDATORY_KEYS: Final[Tuple[str, ...]] = (
     CONFIG_NAME, CONFIG_TYPE, CONFIG_SOURCE)
