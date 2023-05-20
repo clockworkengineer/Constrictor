d6ffa4eec9635e630396366e9df71ab3a7a7e1ec
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-05-20 16:28:25 +0100

    SNapshot.

--- FPE/fpe_windowed.py
+++ FPE/fpe_windowed.py
@@ -8,7 +8,7 @@ from core.engine import Engine
 
 def fpe_windowed(fpe_engine: Engine):
     logging.info("Running with a user interface.")
-    qapp = QApplication(sys.argv)
+    qtapp = QApplication(sys.argv)
     fpe_gui = MainWindow(fpe_engine)
     fpe_gui.show()
-    sys.exit(qapp.exec())     
+    sys.exit(qtapp.exec())     
