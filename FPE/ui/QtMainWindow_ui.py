b63f99f8e42df551e4eb225d742b9e233ed3cbee
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-05-23 19:09:27 +0100

    Snapshot.

--- FPE/ui/QtMainWindow_ui.py
+++ FPE/ui/QtMainWindow_ui.py
@@ -12,7 +12,12 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_fpe_main_window(object):
     def setupUi(self, fpe_main_window):
         fpe_main_window.setObjectName("fpe_main_window")
-        fpe_main_window.resize(800, 600)
+        fpe_main_window.resize(796, 282)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Fixed, QtWidgets.QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(fpe_main_window.sizePolicy().hasHeightForWidth())
+        fpe_main_window.setSizePolicy(sizePolicy)
         self.centralwidget = QtWidgets.QWidget(parent=fpe_main_window)
         self.centralwidget.setObjectName("centralwidget")
         self.running_watchers_groupbox = QtWidgets.QGroupBox(parent=self.centralwidget)
@@ -34,7 +39,7 @@ class Ui_fpe_main_window(object):
         self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
         fpe_main_window.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(parent=fpe_main_window)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 796, 22))
         self.menubar.setObjectName("menubar")
         fpe_main_window.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(parent=fpe_main_window)
