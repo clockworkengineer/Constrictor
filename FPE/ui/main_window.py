d493a996b5a66736a1d37f1bb63c56aa3188e498
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-05-24 20:10:28 +0100

    SNapshot.

--- FPE/ui/main_window.py
+++ FPE/ui/main_window.py
@@ -16,14 +16,17 @@ class MainWindow(QMainWindow, Ui_fpe_main_window):
         self.fpe_watcher_config_textedit.setPlainText(json.dumps(
             self.fpe_engine.running_config()[CONFIG_WATCHERS][row], indent=1))
 
+    def start(self) -> None:
+        current: int = self.fpe_running_watchers_list.currentIndex().row()
+        watcher = self.fpe_running_watchers_list.currentItem().text()
+        if self.fpe_running_watcher_stop_button.text() == "Start":
+            self.fpe_running_watcher_stop_button.setText("Stop")
+            self.fpe_engine.start_watcher(watcher)
+        else:
+            self.fpe_running_watcher_stop_button.setText("Start")
+            self.fpe_engine.stop_watcher(watcher)
+
     def __init__(self, fpe_engine: Engine, parent=None):
-        """_summary_
-
-        Args:
-            fpe_engine (Engine): _description_
-            parent (_type_, optional): _description_. Defaults to None.
-        """
-
         super().__init__(parent)
         self.setupUi(self)
         self.fpe_engine = fpe_engine
@@ -31,7 +34,10 @@ class MainWindow(QMainWindow, Ui_fpe_main_window):
         self.fpe_running_watchers_list.addItems(
             fpe_engine.running_watchers_list())
 
+        self.fpe_running_watchers_list.setCurrentRow(0)
         self.rowChanged(0)
 
         self.fpe_running_watchers_list.currentRowChanged.connect(
             self.rowChanged)
+
+        self.fpe_running_watcher_stop_button.clicked.connect(self.start)
