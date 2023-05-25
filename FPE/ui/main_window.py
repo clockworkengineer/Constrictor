620d85e8aa696e02c5e4251af6a0eb54f26ac642
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-05-25 17:43:33 +0100

    Snapshot.

--- FPE/ui/main_window.py
+++ FPE/ui/main_window.py
@@ -12,14 +12,17 @@ class MainWindow(QMainWindow, Ui_fpe_main_window):
     """Main user interface window.
     """
 
-    def __rowChanged(self, row: int) -> None:
-        self.fpe_watcher_config_textedit.setPlainText(json.dumps(
-            self.fpe_engine.running_config()[CONFIG_WATCHERS][row], indent=1))
+    def __set_start_stop_button_title(self) -> None:
         watcher_name = self.fpe_running_watchers_list.currentItem().text()
         if self.fpe_engine.is_watcher_running(watcher_name):
             self.fpe_running_watcher_start_stop_button.setText("Stop")
         else:
             self.fpe_running_watcher_start_stop_button.setText("Start")
+
+    def __rowChanged(self, row: int) -> None:
+        self.fpe_watcher_config_textedit.setPlainText(json.dumps(
+            self.fpe_engine.running_config()[CONFIG_WATCHERS][row], indent=1))
+        self.__set_start_stop_button_title()
 
     def __start_stop(self) -> None:
         watcher_name = self.fpe_running_watchers_list.currentItem().text()
@@ -27,12 +30,13 @@ class MainWindow(QMainWindow, Ui_fpe_main_window):
             self.fpe_engine.start_watcher(watcher_name)
         else:
             self.fpe_engine.stop_watcher(watcher_name)
-        self.__rowChanged(self.fpe_running_watchers_list.currentRow())
+        self.__set_start_stop_button_title()
 
     def __init__(self, fpe_engine: Engine, parent=None):
-        
+
         super().__init__(parent)
         self.setupUi(self)
+
         self.fpe_engine = fpe_engine
 
         self.fpe_running_watchers_list.addItems(
@@ -44,5 +48,5 @@ class MainWindow(QMainWindow, Ui_fpe_main_window):
         self.fpe_running_watchers_list.currentRowChanged.connect(
             self.__rowChanged)
 
-        self.fpe_running_watcher_start_stop_button.clicked.connect(self.__start_stop)
-
+        self.fpe_running_watcher_start_stop_button.clicked.connect(
+            self.__start_stop)
