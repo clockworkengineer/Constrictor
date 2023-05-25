6b8227f388ab3f8a759e8b16cff4c546e6c2155c
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-05-25 16:42:04 +0100

    SNapshot.

--- FPE/ui/main_window.py
+++ FPE/ui/main_window.py
@@ -12,25 +12,25 @@ class MainWindow(QMainWindow, Ui_fpe_main_window):
     """Main user interface window.
     """
 
-    def rowChanged(self, row: int) -> None:
+    def __rowChanged(self, row: int) -> None:
         self.fpe_watcher_config_textedit.setPlainText(json.dumps(
             self.fpe_engine.running_config()[CONFIG_WATCHERS][row], indent=1))
         watcher_name = self.fpe_running_watchers_list.currentItem().text()
         if self.fpe_engine.is_watcher_running(watcher_name):
-            self.fpe_running_watcher_stop_button.setText("Stop")
+            self.fpe_running_watcher_start_stop_button.setText("Stop")
         else:
-            self.fpe_running_watcher_stop_button.setText("Start")
+            self.fpe_running_watcher_start_stop_button.setText("Start")
 
-    def start(self) -> None:
+    def __start_stop(self) -> None:
         watcher_name = self.fpe_running_watchers_list.currentItem().text()
         if not self.fpe_engine.is_watcher_running(watcher_name):
-            self.fpe_running_watcher_stop_button.setText("Stop")
             self.fpe_engine.start_watcher(watcher_name)
         else:
-            self.fpe_running_watcher_stop_button.setText("Start")
             self.fpe_engine.stop_watcher(watcher_name)
+        self.__rowChanged(self.fpe_running_watchers_list.currentRow())
 
     def __init__(self, fpe_engine: Engine, parent=None):
+        
         super().__init__(parent)
         self.setupUi(self)
         self.fpe_engine = fpe_engine
@@ -39,9 +39,10 @@ class MainWindow(QMainWindow, Ui_fpe_main_window):
             fpe_engine.running_watchers_list())
 
         self.fpe_running_watchers_list.setCurrentRow(0)
-        self.rowChanged(0)
+        self.__rowChanged(0)
 
         self.fpe_running_watchers_list.currentRowChanged.connect(
-            self.rowChanged)
+            self.__rowChanged)
 
-        self.fpe_running_watcher_stop_button.clicked.connect(self.start)
+        self.fpe_running_watcher_start_stop_button.clicked.connect(self.__start_stop)
+
