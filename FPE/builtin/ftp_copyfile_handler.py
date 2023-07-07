bf1f61c055d408c7d01df520846ae4de700ec03e
clockworkengineer <robert_tizzard@hotmail.com>
2023-07-07 08:31:12 +0100

    SFTP -> FTP

--- FPE/builtin/ftp_copyfile_handler.py
+++ FPE/builtin/ftp_copyfile_handler.py
@@ -31,16 +31,16 @@ class FTPCopyFileHandlerError(FPEError):
 
 
 class FTPCopyFileHandler(IHandler):
-    """SFTP Copy file/directories.
+    """FTP Copy file/directories.
 
-    SFTP Copy files created in watch folder to destination folder on remote SSH server.
+    FTP Copy files created in watch folder to destination folder on remote SSH server.
 
     Attributes:
         name:           Name of handler object
         source:         Folder to watch for files
-        server:         SSH Server
-        user:           SSH Server username
-        password:       SSH Server user password
+        server:         FTP Server
+        user:           FTP Server username
+        password:       FTP Server user password
         destination:    Destination for copy
         deletesource:   Boolean == true delete source file on success
         exitonfailure:  Boolean == true exit handler on failure; generating an exception
@@ -70,7 +70,7 @@ class FTPCopyFileHandler(IHandler):
         logging.getLogger("paramiko").setLevel(logging.WARNING)
 
     def process(self,  source_path: pathlib.Path) -> bool:
-        """SFTP Copy file from source(watch) directory to a destination directory on remote server.
+        """FTP Copy file from source(watch) directory to a destination directory on remote server.
 
         Args:
             source_path (pathlib.Path): Source file path.
@@ -84,19 +84,19 @@ class FTPCopyFileHandler(IHandler):
             destination_path: pathlib.Path = Handler.create_local_destination(
                 source_path, self.handler_config)
 
-            # with pysftp.Connection(self.handler_config["server"], username=self.handler_config["user"],
-            #                        password=self.handler_config["password"]) as sftp:
+            # with pyFTP.Connection(self.handler_config["server"], username=self.handler_config["user"],
+            #                        password=self.handler_config["password"]) as FTP:
             #     if source_path.is_file():
-            #         sftp.put(source_path, destination_path)
+            #         FTP.put(source_path, destination_path)
             #     else:
-            #         sftp.makedirs(destination_path)
+            #         FTP.makedirs(destination_path)
             
             with FTP(host=self.handler_config["server"], user=self.handler_config["user"], passwd=self.handler_config["password"]) as ftp:
                 if source_path.is_file():
                     with open(source_path, 'rb') as file:
                         ftp.storbinary(f'STOR {source_path.name}', file)
             #     else:
-            #         sftp.makedirs(destination_path)
+            #         FTP.makedirs(destination_path)
 
             logging.info("Uploaded file %s to %s",
                          source_path, destination_path)
