b6658985deac3f06abb6f7089b0184107228679b
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-09-08 20:03:42 +0100

    Add failure callback to engine.

--- FPE/core/engine.py
+++ FPE/core/engine.py
@@ -2,6 +2,7 @@
 """
 
 import json
+from typing import Callable
 
 from builtin.handler_list import fpe_handler_list
 from core.constants import CONFIG_NAME, CONFIG_WATCHERS, CONFIG_FILENAME, CONFIG_NOGUI
@@ -12,11 +13,11 @@ from core.plugin import PluginLoader
 
 
 class Engine:
-    """Control class for the FPE used to create, control and delete directory watchers.
-    """
+    """Control class for the FPE used to create, control and delete directory watchers."""
 
     __engine_config: ConfigDict = {}
     __engine_watchers: dict[str, Watcher] = {}
+    __engine_watcher_failure_callback: Callable[..., None] = None
 
     def __init__(self, engine_config: ConfigDict) -> None:
         """Create FPE engine.
@@ -34,7 +35,7 @@ class Engine:
         for handler_name, handler in fpe_handler_list.items():
             Factory.register(handler_name, handler)
 
-        PluginLoader.load(self.__engine_config['plugins'])
+        PluginLoader.load(self.__engine_config["plugins"])
 
     def create_watcher(self, watcher_config: ConfigDict) -> None:
         """Create a directory watcher.
@@ -44,8 +45,7 @@ class Engine:
         """
         current_watcher = Watcher(watcher_config)
         if current_watcher is not None:
-            self.__engine_watchers[watcher_config[CONFIG_NAME]
-                                   ] = current_watcher
+            self.__engine_watchers[watcher_config[CONFIG_NAME]] = current_watcher
 
     def delete_watcher(self, watcher_name: str) -> None:
         """Delete directory watcher.
@@ -90,8 +90,7 @@ class Engine:
         return self.__engine_watchers[watcher_name].is_running
 
     def startup(self) -> None:
-        """Create directory watchers from config and startup.
-        """
+        """Create directory watchers from config and startup."""
 
         for watcher_config in self.__engine_config[CONFIG_WATCHERS]:
             self.create_watcher(watcher_config)
@@ -100,8 +99,7 @@ class Engine:
             self.start_watcher(watcher_name)
 
     def shutdown(self) -> None:
-        """Shutdown watchers created by engine.
-        """
+        """Shutdown watchers created by engine."""
         for watcher_name, watcher in self.__engine_watchers.items():
             self.stop_watcher(watcher_name)
             watcher.stop()
@@ -109,15 +107,13 @@ class Engine:
         self.__engine_watchers.clear()
 
     def running_watchers_list(self) -> list[str]:
-        """Return list of current watcher names.
-        """
+        """Return list of current watcher names."""
         return list(self.__engine_watchers.keys())
 
     def running_config(self) -> ConfigDict:
-        """Return engine configuration.
-        """
+        """Return engine configuration."""
         return self.__engine_config
-    
+
     def return_watcher(self, watcher_name: str) -> Watcher:
         """Is a named watcher running ?
 
@@ -131,13 +127,22 @@ class Engine:
         return self.__engine_watchers[watcher_name]
 
     def save_config(self) -> None:
-        """Save current configuration away to JSON file.
-        """
+        """Save current configuration away to JSON file."""
         # Copy engine config
         config_to_save: ConfigDict = self.__engine_config.copy()
         # Remove unneeded keys
         config_to_save.pop(CONFIG_FILENAME)
         config_to_save.pop(CONFIG_NOGUI)
         # Write JSON configuration
-        with open(self.__engine_config[CONFIG_FILENAME], "w", encoding="utf-8") as json_file:
+        with open(
+            self.__engine_config[CONFIG_FILENAME], "w", encoding="utf-8"
+        ) as json_file:
             json_file.write(json.dumps(config_to_save, indent=1))
+
+    def set_failure_callback(self, failure_callback_fn: Callable[..., None]) -> None:
+        """_summary_
+
+        Args:
+            failure_callback_fn (Callable[..., None]): _description_
+        """
+        self.__engine_watcher_failure_callback = failure_callback_fn
