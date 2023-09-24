caf88ea7ab7c13a24c982b7e67b445402135f483
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-09-24 18:12:21 +0100

    Add placeholers files for more tests to be added.

--- FPE/core/arguments.py
+++ FPE/core/arguments.py
@@ -33,7 +33,7 @@ class Arguments:
     # Parsed argument attributes
 
     file: str   # Configuration file name
-    nogui: bool # Running with GUI flag 
+    nogui: bool # Running with no GUI flag 
 
     def __init__(self, argv=None) -> None:
         """Load and parse command line into arguments object.
