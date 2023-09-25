f56450b129b19d1a223a2e72c9710e856ea67348
clockworkengineer <robert_tizzard@hotmail.com>
2023-09-25 08:38:12 +0100

    Add test ideas

--- FPE/tests/test_core_consumer.py
+++ FPE/tests/test_core_consumer.py
@@ -2,3 +2,13 @@ import pytest
 
 class TestCoreConsumer:
     pass
+
+# test for valid creation.
+# test for invalid queue
+# test for invalid ihandler
+# test for invalid failure function
+# test for start when in initial state
+# test for start stop
+# test for start start
+# test for stop stop
+# test for stop when not started
