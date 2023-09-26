1ab7ec3b3f083fc8269b96e560952bd23ddc5374
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-09-26 18:05:40 +0100

    Add more test ideas.

--- FPE/tests/test_core_engine.py
+++ FPE/tests/test_core_engine.py
@@ -9,3 +9,7 @@ class TestCoreEngine:
 # Test valida engine config
 # Test no handlers in config works ok
 # Test empty plugins works ok
+# Test to create a watcher
+# Test to delete a watcher
+# Test to start a watcher
+# Test to stop a watcher
