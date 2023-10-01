79090ae65646c0cc729d8becf798aa59798f7989
clockworkengineer <robert_tizzard@hotmail.com>
2023-10-01 09:17:42 +0100

    Add more pylint changes.

--- FPE/tests/test_builtin_csvfile_to_sql_handler.py
+++ FPE/tests/test_builtin_csvfile_to_sql_handler.py
@@ -3,6 +3,10 @@
 
 import pytest
 
+"""TEST"""
+# pylint: disable=missing-function-docstring, missing-class-docstring, unused-argument
+
+import pytest
 
 class TestBuiltinCSVFileToSQLHandler:
     pass
